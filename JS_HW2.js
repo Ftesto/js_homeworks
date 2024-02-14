d6fdf937e7f64cb469b4d6761d6592148e6cf49d
FTesto <ftestillio@gmail.com>
2024-02-14 21:35:52 +0200

    вторая домашка по жсу

---
+++ JS_HW2.js
@@ -0,0 +1,76 @@
+var year = prompt("Вкажіть кількіть років", "Кількість років числом");
+
+if (!isNaN(year) && year >= 1) {
+    if (year == 1) {
+        alert(year + " рік");
+    } 
+    else if (year >= 2 && year <= 4) {
+        alert(year + " роки");
+    }
+    else if (year == 21) {
+        alert(year + " рік");
+    } 
+    else if (year >= 22 && year <= 24) {
+        alert(year + " роки");
+    }
+    else if (year == 31) {
+        alert(year + " рік");
+    } 
+    else if (year >= 32 && year <= 34) {
+        alert(year + " роки");
+    }
+    else if (year == 41) {
+        alert(year + " рік");
+    } 
+    else if (year >= 42 && year <= 44) {
+        alert(year + " роки");
+    }
+    else if (year == 51) {
+        alert(year + " рік");
+    } 
+    else if (year >= 52 && year <= 54) {
+        alert(year + " роки");
+    }
+    else if (year == 61) {
+        alert(year + " рік");
+    } 
+    else if (year >= 62 && year <= 64) {
+        alert(year + " роки");
+    }
+    else if (year == 71) {
+        alert(year + " рік");
+    } 
+    else if (year >= 72 && year <= 74) {
+        alert(year + " роки");
+    }
+    else if (year == 81) {
+        alert(year + " рік");
+    } 
+    else if (year >= 82 && year <= 84) {
+        alert(year + " роки");
+    }
+    else if (year == 91) {
+        alert(year + " рік");
+    } 
+    else if (year >= 92 && year <= 94) {
+        alert(year + " роки");
+    }
+    else if (year == 101) {
+        alert(year + " рік");
+    } 
+    else if (year >= 102 && year <= 104) {
+        alert(year + " роки");
+    }
+    else if (year == 121) {
+        alert(year + " рік");
+    } 
+    else if (year >= 122 && year <= 124) {
+        alert(year + " роки");
+    }
+    else {
+        alert(year + " років");
+    }
+} 
+else {
+    alert("Будь ласка, введіть додатне число.");
+}
